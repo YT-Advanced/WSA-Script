2fc73595a94079caa979e49733c5d54307030711
s1204IT <pub@s1204.me>
2023-05-30 23:39:29 +0900

    :laughing: Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -605,7 +605,7 @@ else
 fi
 cp ../installer/Install.ps1 "$WORK_DIR/wsa/$ARCH" || abort
 find "$WORK_DIR/wsa/$ARCH" -not -path "*/pri*" -not -path "*/xml*" -printf "%P\n" | sed -e 's/\//\\/g' -e '/^$/d' > "$WORK_DIR/wsa/$ARCH/filelist.txt" || abort
-find "$WORK_DIR/pri/$ARCH/pri" -printf "%P\n" | sed -e 's/^/pri\\/' -e '/^$/d' > "$WORK_DIR/wsa/$ARCH/filelist-pri.txt" || abort
+find "$WORK_DIR/wsa/$ARCH/pri" -printf "%P\n" | sed -e 's/^/pri\\/' -e '/^$/d' > "$WORK_DIR/wsa/$ARCH/filelist-pri.txt" || abort
 find "$WORK_DIR/wsa/$ARCH/xml" -printf "%P\n" | sed -e 's/^/xml\\/' -e '/^$/d' >> "$WORK_DIR/wsa/$ARCH/filelist-pri.txt" || abort
 cp "../bin/$ARCH/makepri.exe" "$WORK_DIR/wsa/$ARCH" || abort
 echo "makepri.exe" >> "$WORK_DIR/wsa/$ARCH/filelist-pri.txt" || abort
