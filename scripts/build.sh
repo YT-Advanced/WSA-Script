66ef6ec18f15b5e905b988ca4d6893080b590ca8
s1204IT <pub@s1204.me>
2023-05-30 21:11:24 +0900

    Try to fix: filelist.txt

--- scripts/build.sh
+++ scripts/build.sh
@@ -611,7 +611,7 @@ else
 fi
 cp ../installer/Install.ps1 "$WORK_DIR/wsa/$ARCH" || abort
 cp ../installer/Run.bat "$WORK_DIR/wsa/$ARCH" || abort
-find "$WORK_DIR/wsa/$ARCH" -maxdepth 1 -mindepth 1 -printf "%P\n" >"$WORK_DIR/wsa/$ARCH/filelist.txt" || abort
+find "$WORK_DIR/wsa/$ARCH" -printf "%P\n" | sed -e 's/\//\\/g' >"$WORK_DIR/wsa/$ARCH/filelist.txt" || abort
 echo -e "Remove signature and add scripts done\n"
 
 echo "Generate info"
