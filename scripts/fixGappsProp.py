b1a3211fe479a02d1ed2b1357c7f412c268adf2a
Syuugo <pub@s1204.me>
2023-06-04 14:44:03 +0900

    Try to fix missing arguments

--- scripts/fixGappsProp.py
+++ scripts/fixGappsProp.py
@@ -41,18 +41,18 @@ class Prop(OrderedDict):
         return self
 
 new_props = {
-    ("product", "brand"): "sys.argv[2]",        # Default is google
-    ("system", "brand"): "sys.argv[2]",
-    ("product", "manufacturer"): "sys.argv[3]", # Default is Google
-    ("system", "manufacturer"): "sys.argv[3]",
-    ("build", "product"): "sys.argv[4]",        # Default is redfin
-    ("product", "name"): "sys.argv[4]",         # Default is redfin
-    ("system", "name"): "sys.argv[4]",
-    ("product", "device"): "sys.argv[4]",       # Default is redfin
-    ("system", "device"): "sys.argv[4]",
-    ("product", "model"): "sys.argv[5]",        # Default is Pixel 5
-    ("system", "model"): "sys.argv[5]",
-    ("build", "flavor"): "sys.argv[4]-user"     # Default is redfin-user
+    ("product", "brand"): sys.argv[2],         # Default is google
+    ("system", "brand"): sys.argv[2],
+    ("product", "manufacturer"): sys.argv[3],  # Default is Google
+    ("system", "manufacturer"): sys.argv[3],
+    ("build", "product"): sys.argv[4],         # Default is redfin
+    ("product", "name"): sys.argv[4],          # Default is redfin
+    ("system", "name"): sys.argv[4],
+    ("product", "device"): sys.argv[4],        # Default is redfin
+    ("system", "device"): sys.argv[4],
+    ("product", "model"): sys.argv[5],         # Default is Pixel 5
+    ("system", "model"): sys.argv[5],
+    ("build", "flavor"): sys.argv[4] + "-user" # Default is redfin-user
 }
 
 def description(sec: str, p: Prop) -> str:
