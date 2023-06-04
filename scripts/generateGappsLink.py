31acf05cfc895040b3b5611d56365729d8953a26
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-06-04 19:50:05 +0700

    Escape dot

--- scripts/generateGappsLink.py
+++ scripts/generateGappsLink.py
@@ -35,7 +35,7 @@ print(f"Generating MindTheGapps download link: arch={arch}", flush=True)
 abi_map = {"x64": "x86_64", "arm64": "arm64"}
 res = requests.get(
     f'https://sourceforge.net/projects/wsa-mtg/rss?path=/{abi_map[arch]}&limit=100')
-matched = re.search(f'https://.*13.0.0*{abi_map[arch]}.*\.zip/download', res.text)
+matched = re.search(f'https://.*13[.]0[.]0-{abi_map[arch]}-*[.]zip/download', res.text)
 if matched:
     link = matched.group().replace(
         '.zip/download', '.zip').replace('sourceforge.net/projects/wsa-mtg/files', 'downloads.sourceforge.net/project/wsa-mtg')
@@ -48,7 +48,7 @@ else:
     if res.status_code == 200:
         assets = json_data["assets"]
         for asset in assets:
-            if re.match(f'.*13.0.0*{abi_map[arch]}.*\.zip$', asset["name"]) and asset["content_type"] == "application/zip":
+            if re.match(f'.*13[.]0[.]0-{abi_map[arch]}-*\.zip$', asset["name"]) and asset["content_type"] == "application/zip":
                 link = asset["browser_download_url"]
                 break
     elif res.status_code == 403 and x_ratelimit_remaining == '0':
