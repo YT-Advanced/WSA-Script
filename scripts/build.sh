c67b8fd7e76795444662950a05f5efb0821db1b6
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-06-24 16:23:42 +0700

    Remove unused code
    Since #78, we don't need these code

--- scripts/build.sh
+++ scripts/build.sh
@@ -377,7 +377,6 @@ if [ "$GAPPS_BRAND" != 'none' ]; then
         fi
         mv "$WORK_DIR/gapps/system/"* "$WORK_DIR/gapps" || abort
         rm -rf "${WORK_DIR:?}/gapps/system" || abort
-        cp -r "../common/gapps/"* "$WORK_DIR/gapps" || abort
     else
         abort "The MindTheGapps zip package does not exist."
     fi
@@ -536,18 +535,8 @@ EOF
     echo -e "Add KernelSU Manager done\n"
 fi
 
-echo "Add extra packages"
-sudo cp -r "../common/system/"* "$SYSTEM_MNT" || abort
-if [[ -d "../common/system/priv-app/" ]]; then
-    find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo rm -rf "$SYSTEM_MNT/priv-app/placeholder/oat"
-    find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -type d -exec chmod 0755 {} \;
-    find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -type f -exec chmod 0644 {} \;
-    find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -exec chown root:root {} \;
-    find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -exec setfattr -n security.selinux -v "u:object_r:system_file:s0" {} \; || abort
-fi
-echo -e "Add extra packages done\n"
-
 echo "Permissions management Netfree and Netspark security certificates"
+sudo cp -r "../common/system/"* "$SYSTEM_MNT" || abort
 find "../common/system/etc/security/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/etc/security/placeholder" -type d -exec chmod 0755 {} \;
 find "../common/system/etc/security/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/etc/security/placeholder" -type f -exec chmod 0644 {} \;
 find "../common/system/etc/security/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/etc/security/placeholder" -exec chown root:root {} \;
