bdfaaa7782c60759f460b5ee89ed4f4e710f10ee
Syuugo <pub@s1204.me>
2023-07-03 21:00:13 +0900

    int

--- scripts/build.sh
+++ scripts/build.sh
@@ -270,7 +270,7 @@ GAPPS_PATH=$DOWNLOAD_DIR/$GAPPS_ZIP_NAME
 
 update_ksu_zip_name() {
     KERNEL_VER="5.15.94.4"
-    if [ "$WSA_MAJOR_VER" -ge "2306" ]; then
+    if [ $WSA_MAJOR_VER >= 2306 ]; then
         KERNEL_VER="5.15.104.1"
     fi
     KERNELSU_ZIP_NAME=kernelsu-$ARCH-$KERNEL_VER.zip
