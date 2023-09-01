6f605bd497aeb894e2e249d1fdc1967a450c40b3
Syuugo <pub@s1204.me>
2023-09-01 14:10:45 +0900

    Add Pixel devices
    - Pixel 7a
    - Pixel Tablet

--- scripts/build.sh
+++ scripts/build.sh
@@ -189,6 +189,8 @@ CUSTOM_MODEL_MAP=(
     "bluejay"
     "panther"
     "cheetah"
+    "lynx"
+    "tangorpro"
 )
 
 ROOT_SOL_MAP=(
@@ -670,17 +672,17 @@ fi
 if [[ "$CUSTOM_MODEL" != "none" ]]; then
     echo "Fix system props"
     # The first argument is prop path, second is product name (redfin), third is device model (Pixel 5)
-    declare -A MODEL_NAME_MAP=(["sunfish"]="Pixel 4a" ["bramble"]="Pixel 4a (5G)" ["redfin"]="Pixel 5" ["barbet"]="Pixel 5a" ["raven"]="Pixel 6 Pro" ["oriole"]="Pixel 6" ["bluejay"]="Pixel 6a" ["panther"]="Pixel 7" ["cheetah"]="Pixel 7 Pro")
+    declare -A MODEL_NAME_MAP=(["sunfish"]="Pixel 4a" ["bramble"]="Pixel 4a (5G)" ["redfin"]="Pixel 5" ["barbet"]="Pixel 5a" ["raven"]="Pixel 6 Pro" ["oriole"]="Pixel 6" ["bluejay"]="Pixel 6a" ["panther"]="Pixel 7" ["cheetah"]="Pixel 7 Pro" ["lynx"]="Pixel 7a" ["tangorpro"]="Pixel Tablet")
     MODEL_NAME="${MODEL_NAME_MAP[$CUSTOM_MODEL]}"
     sudo python3 fixGappsProp.py "$ROOT_MNT" "$CUSTOM_MODEL" "$MODEL_NAME" || abort
     # shellcheck disable=SC2002
-    BUILD_ID=$(sudo cat "$SYSTEM_MNT/build.prop" | grep -e ro.build.id | cut -c 13-)
-    if [[ "${#BUILD_ID}" != "15" ]]; then
-        FIXED_BUILD_ID=$(echo "$BUILD_ID" | cut -c -15)
-        echo "Fixed Build ID: $FIXED_BUILD_ID"
-        # shellcheck disable=SC2086
-        sudo find $ROOT_MNT/{system,system_ext,product,vendor} -name "build.prop" -exec sed -i -e "s/$BUILD_ID/$FIXED_BUILD_ID/g" {} \;
-    fi
+    #BUILD_ID=$(sudo cat "$SYSTEM_MNT/build.prop" | grep -e ro.build.id | cut -c 13-)
+    #if [[ "${#BUILD_ID}" != "15" ]]; then
+    #    FIXED_BUILD_ID=$(echo "$BUILD_ID" | cut -c -15)
+    #    echo "Fixed Build ID: $FIXED_BUILD_ID"
+    #    # shellcheck disable=SC2086
+    #    sudo find $ROOT_MNT/{system,system_ext,product,vendor} -name "build.prop" -exec sed -i -e "s/$BUILD_ID/$FIXED_BUILD_ID/g" {} \;
+    #fi
     echo -e "done\n"
     MODEL_NAME="${MODEL_NAME// /-}"
 else
