94a1292209a063600e3fd820f889ffeb97c04157
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-03-03 11:44:58 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -402,7 +402,7 @@ cp "$UWPVCLibs_PATH" "$xaml_PATH" "$WORK_DIR/wsa/$ARCH" || abort
 cp "../xml/priconfig.xml" "$WORK_DIR/wsa/$ARCH/xml/" || abort
 cp ../installer/$ARCH/makepri.exe "$WORK_DIR/wsa/$ARCH" || abort
 cp ../installer/$ARCH/MakePri.ps1 "$WORK_DIR/wsa/$ARCH" || abort
-cp ../installer/Install.ps1 "$WORK_DIR/wsa/$ARCH" || abort
+cp ../installer/$ARCH/Install.ps1 "$WORK_DIR/wsa/$ARCH" || abort
 cp ../installer/Run.bat "$WORK_DIR/wsa/$ARCH" || abort
 find "$WORK_DIR/wsa/$ARCH" -maxdepth 1 -mindepth 1 -printf "%P\n" >"$WORK_DIR/wsa/$ARCH/filelist.txt" || abort
 echo -e "done\n"
