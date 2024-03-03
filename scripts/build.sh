6de9cc3cc2c9b4ec2de700ff8a4236d6adba0d8b
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-03-03 11:47:10 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -400,10 +400,10 @@ rm -rf "${WORK_DIR:?}"/wsa/"$ARCH"/\[Content_Types\].xml "$WORK_DIR/wsa/$ARCH/Ap
 cp "$vclibs_PATH" "$xaml_PATH" "$WORK_DIR/wsa/$ARCH" || abort
 cp "$UWPVCLibs_PATH" "$xaml_PATH" "$WORK_DIR/wsa/$ARCH" || abort
 cp "../xml/priconfig.xml" "$WORK_DIR/wsa/$ARCH/xml/" || abort
-cp ../installer/$ARCH/makepri.exe "$WORK_DIR/wsa/$ARCH" || abort
-cp ../installer/$ARCH/MakePri.ps1 "$WORK_DIR/wsa/$ARCH" || abort
-cp ../installer/$ARCH/Install.ps1 "$WORK_DIR/wsa/$ARCH" || abort
-cp ../installer/Run.bat "$WORK_DIR/wsa/$ARCH" || abort
+cp "../installer/$ARCH/makepri.exe" "$WORK_DIR/wsa/$ARCH" || abort
+cp "../installer/$ARCH/MakePri.ps1" "$WORK_DIR/wsa/$ARCH" || abort
+cp "../installer/$ARCH/Install.ps1" "$WORK_DIR/wsa/$ARCH" || abort
+cp "../installer/Run.bat" "$WORK_DIR/wsa/$ARCH" || abort
 find "$WORK_DIR/wsa/$ARCH" -maxdepth 1 -mindepth 1 -printf "%P\n" >"$WORK_DIR/wsa/$ARCH/filelist.txt" || abort
 echo -e "done\n"
 
