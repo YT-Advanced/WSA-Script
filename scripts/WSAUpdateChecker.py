f93422d199a857d97a8a9fb635f113e28a9664e3
Notch Apple <Gapple1703@gmail.com>
2024-07-19 17:56:23 +0700

    abcxyz

--- scripts/WSAUpdateChecker.py
+++ scripts/WSAUpdateChecker.py
@@ -1,217 +1,299 @@
 import base64
-from os import getenv
 import html
-import json
-import re
-import requests
 import logging
+import os
+import re
 import subprocess
 
-from typing import Any, OrderedDict
-from xml.dom import minidom
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Final, Literal
+
+import requests
+import xmltodict
 
 from requests import Session
-from packaging import version
 
-class Prop(OrderedDict):
-    def __init__(self, props: str = ...) -> None:
-        super().__init__()
-        for i, line in enumerate(props.splitlines(False)):
-            if '=' in line:
-                k, v = line.split('=', 1)
-                self[k] = v
-            else:
-                self[f".{i}"] = line
-
-    def __setattr__(self, __name: str, __value: Any) -> None:
-        self[__name] = __value
-
-    def __repr__(self):
-        return '\n'.join(f'{item}={self[item]}' for item in self)
 
 logging.captureWarnings(True)
-env_file = getenv('GITHUB_ENV')
-token = getenv("API_KEY")
-authorization = f'Bearer {token}'
-reqheaders = {
-    "Accept": "application/vnd.github.v3+json",
-    "Authorization" : authorization,
-}
-#Catagory ID
-cat_id = '858014f3-3934-4abe-8078-4aa193e74ca8'
-
-release_type = "WIF"
-
-new_version_found = False
 
 session = Session()
 session.verify = False
 
-git = (
+
+ENV_FILE_PATH: Final[Path] = Path(str(os.getenv("GITHUB_ENV")))
+
+GIT_COMMAND: Final[str] = (
     "git checkout -f update || git switch --discard-changes --orphan update"
 )
 
-def Magisk_Ksu_Gapps_Checker(type):
-    global new_version_found
-    # Get current version
-    currentver = requests.get(f"https://raw.githubusercontent.com/YT-Advanced/WSA-Script/update/" + type + ".appversion").text.replace('\n', '')
-    # Write for pushing later
-    file = open('../' + type + '.appversion', 'w')
-    file.write(currentver)
-    if new_version_found:
-        return 0;
+CATEGORY_ID: Final[str] = "858014f3-3934-4abe-8078-4aa193e74ca8"
 
-    # Get latest version
-    latestver = ""
-    msg = ""
-    if (type == "magisk"):
-        latestver = json.loads(requests.get(f"https://github.com/topjohnwu/magisk-files/raw/master/stable.json").content)['magisk']['version'].replace('\n', '')
-        msg="Update Magisk Version from `v" + currentver + "` to `v" + latestver + "`"
-    elif (type == "gapps"):
-        latestver = json.loads(requests.get(f"https://api.github.com/repos/YT-Advanced/MindTheGappsBuilder/releases/latest", headers=reqheaders).content)['name']
-        msg="Update MindTheGapps Version from `v" + currentver + "` to `v" + latestver + "`"
-    elif (type == "kernelsu"):
-        latestver = json.loads(requests.get(f"https://api.github.com/repos/tiann/KernelSU/releases/latest", headers=reqheaders).content)['name']
-        msg="Update KernelSU Version from `" + currentver + "` to `" + latestver + "`"
 
-    # Check if version is the same or not
-    if (currentver != latestver):
-        print("New version found: " + latestver)
-        new_version_found = True
-        # Write appversion content
-        subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
-        file.seek(0)
-        file.truncate()
-        file.write(latestver)
-        # Write Github Environment
-        with open(env_file, "a") as wr:
-            wr.write("SHOULD_BUILD=yes\nMSG=" + msg)
-    file.close()
+# Thanks to bubbles-wow's repository
+def get_usercode() -> str:
+    response = requests.get(
+        "https://api.github.com/repos/bubbles-wow/WSAUpdateChecker/contents/token.conf"
+    )
 
-def WSAChecker(user, release_type):
-    global new_version_found
-    currentver = requests.get(f"https://raw.githubusercontent.com/YT-Advanced/WSA-Script/update/" + release_type + ".appversion").text.replace('\n', '')
+    content = response.json()["content"]
+    content = content.encode()
+    content = base64.b64decode(content)
+    content = content.decode()
 
-    # Write for pushing later
-    file = open('../' + release_type + '.appversion', 'w')
-    file.write(currentver)
+    user_code: str = re.search("(?<=user_code=).*", content).group()  # type: ignore
 
-    if new_version_found:
-        return 0;
-    # Get information
-    with open("../xml/GetCookie.xml", "r") as f:
-        cookie_content = f.read().format(user)
-        f.close()
-    try:
-        out = session.post(
-            'https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx',
-            data=cookie_content,
-            headers={'Content-Type': 'application/soap+xml; charset=utf-8'}
+    return user_code
+
+
+class UpdateChecker(ABC):
+    def __init__(self, app_name: str):
+        self.app_name = app_name
+
+        self.current_version: str
+        self.latest_version: str
+
+    @property
+    def update_msg(self) -> str:
+        return f"Update {self.app_name} Version from 'v{self.current_version}' to 'v{self.latest_version}'"
+
+    @abstractmethod
+    def get_current_version(self) -> str: ...
+
+    @abstractmethod
+    def get_latest_version(self) -> str: ...
+
+    def get_update(self) -> None:
+        if self.is_up_to_date():
+            return
+
+        self.update()
+        self.write_to_github_environment()
+        self.overwrite_current_version()
+
+    def is_up_to_date(self) -> bool:
+        return self.current_version == self.latest_version
+
+    def update(self) -> None:
+        print(f"New version found: {self.latest_version}")
+
+        subprocess.Popen(
+            GIT_COMMAND,
+            shell=True,
+            stdout=None,
+            stderr=None,
+            executable="/bin/bash",
+        ).wait()
+
+    @abstractmethod
+    def write_to_github_environment(self) -> None: ...
+
+    @abstractmethod
+    def overwrite_current_version(self) -> None: ...
+
+
+class UtilUpdateChecker(UpdateChecker):
+    def __init__(
+        self,
+        app_id: str,
+        app_name: str,
+        latest_version_url: str,
+        json_keys: tuple[str, ...],
+    ) -> None:
+
+        super().__init__(app_name)
+
+        self.app_id = app_id
+        self.latest_version_url = latest_version_url
+        self.json_keys = json_keys
+
+        self.current_version = self.get_current_version()
+        self.latest_version = self.get_latest_version()
+
+    @property
+    def current_version_url(self) -> str:
+        return f"https://raw.githubusercontent.com/YT-Advanced/WSA-Script/update/{self.app_id}.appversion"
+
+    @property
+    def current_version_file_path(self) -> Path:
+        return Path(f"../{self.app_id}.appversion")
+
+    def get_current_version(self) -> str:
+        response = requests.get(self.current_version_url)
+
+        return response.text
+
+    def get_latest_version(self) -> str:
+        response = requests.get(self.latest_version_url)
+        content = response.json()
+
+        for key in self.json_keys:
+            content = content.get(key)
+
+        return content
+
+    def write_to_github_environment(self) -> None:
+        with open(ENV_FILE_PATH, "a") as env_file:
+            env_file.write(
+                f"""
+                SHOULD_BUILD=yes
+                MSG={self.update_msg}
+                """.strip()
+            )
+
+    def overwrite_current_version(self) -> None:
+        with open(self.current_version_file_path, "w") as current_version_file:
+            current_version_file.write(self.latest_version)
+
+
+class WSAUpdateChecker(UpdateChecker):
+    def __init__(self, app_name: str, update_channel: Literal["retail", "WIF"]) -> None:
+        super().__init__(app_name)
+
+        self.update_channel = update_channel
+
+        self.current_version = self.get_current_version()
+        self.latest_version = self.get_latest_version()
+
+    @property
+    def current_version_url(self) -> str:
+        return f"https://raw.githubusercontent.com/YT-Advanced/WSA-Script/update/{self.update_channel}.appversion"
+
+    @property
+    def current_version_file_path(self) -> Path:
+        return Path(f"../{self.update_channel}.appversion")
+
+    @property
+    def product_id(self) -> str: # type: ignore
+        if self.update_channel == "retail":
+            return "301870105"
+        elif self.update_channel == "WIF":
+            return "301870107"
+
+    def get_current_version(self) -> str:
+        response = requests.get(self.current_version_url)
+
+        return response.text
+
+    def get_latest_version(self) -> str: # type: ignore
+        user_code: str = get_usercode()
+        # retail: 301870105
+        # WIF:    301870107
+
+        # user_code: str = ''
+        # retail: 301870114
+        # WIF:    301870124
+
+        with open("../xml/GetCookie.xml", "r") as cookie_file:
+            cookie = cookie_file.read()
+            cookie = cookie.format(user_code)
+
+        response = session.post(
+            "https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx",
+            headers={"Content-Type": "application/soap+xml; charset=utf-8"},
+            data=cookie,
         )
-    except:
-        print("Network Error!")
-        return 1
-    doc = minidom.parseString(out.text)
-    cookie = doc.getElementsByTagName('EncryptedData')[0].firstChild.nodeValue
-    with open("../xml/WUIDRequest.xml", "r") as f:
-        cat_id_content = f.read().format(user, cookie, cat_id, release_type)
-        f.close()
-    try:
-        out = session.post(
-            'https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx',
-            data=cat_id_content,
-            headers={'Content-Type': 'application/soap+xml; charset=utf-8'}
+
+        content = xmltodict.parse(response.text)
+        token = content["s:Envelope"]["s:Body"]["GetCookieResponse"] \
+                       ["GetCookieResult"]["EncryptedData"].strip("'")
+
+        with open("../xml/WUIDRequest.xml", "r") as cookie_file:
+            cookie = cookie_file.read()
+            cookie = cookie.format(user_code, token, CATEGORY_ID, "WIF")
+
+        response = session.post(
+            "https://fe3.delivery.mp.microsoft.com/ClientWebService/client.asmx",
+            headers={"Content-Type": "application/soap+xml; charset=utf-8"},
+            data=cookie,
         )
-    except:
-        print("Network Error!")
-        return 1
-    doc = minidom.parseString(html.unescape(out.text))
-    filenames = {}
-    for node in doc.getElementsByTagName('ExtendedUpdateInfo')[0].getElementsByTagName('Updates')[0].getElementsByTagName('Update'):
-        node_xml = node.getElementsByTagName('Xml')[0]
-        node_files = node_xml.getElementsByTagName('Files')
-        if not node_files:
-            continue
-        else:
-            for node_file in node_files[0].getElementsByTagName('File'):
-                if node_file.hasAttribute('InstallerSpecificIdentifier') and node_file.hasAttribute('FileName'):
-                    filenames[node.getElementsByTagName('ID')[0].firstChild.nodeValue] = (f"{node_file.attributes['InstallerSpecificIdentifier'].value}_{node_file.attributes['FileName'].value}",
-                                                                                          node_xml.getElementsByTagName('ExtendedProperties')[0].attributes['PackageIdentityName'].value)
-    identities = {}
-    for node in doc.getElementsByTagName('NewUpdates')[0].getElementsByTagName('UpdateInfo'):
-        node_xml = node.getElementsByTagName('Xml')[0]
-        if not node_xml.getElementsByTagName('SecuredFragment'):
-            continue
-        else:
-            id = node.getElementsByTagName('ID')[0].firstChild.nodeValue
-            update_identity = node_xml.getElementsByTagName('UpdateIdentity')[0]
-            if id in filenames:
-                fileinfo = filenames[id]
-                if fileinfo[0] not in identities:
-                    identities[fileinfo[0]] = ([update_identity.attributes['UpdateID'].value,
-                                            update_identity.attributes['RevisionNumber'].value], fileinfo[1])
-    wsa_build_ver = 0
-    for filename, value in identities.items():
-        if re.match(f"MicrosoftCorporationII.WindowsSubsystemForAndroid_.*.msixbundle", filename):
-            tmp_wsa_build_ver = re.search(r"\d{4}.\d{5}.\d{1,}.\d{1,}", filename).group()
-            if (wsa_build_ver == 0):
-                wsa_build_ver = tmp_wsa_build_ver
-            elif version.parse(wsa_build_ver) < version.parse(tmp_wsa_build_ver):
-                wsa_build_ver = tmp_wsa_build_ver
-     
-    # Check new WSA version
-    if version.parse(currentver) < version.parse(wsa_build_ver):
-        print("New version found: " + wsa_build_ver)
-        new_version_found = True
-        # Write appversion content
-        subprocess.Popen(git, shell=True, stdout=None, stderr=None, executable='/bin/bash').wait()
-        file.seek(0)
-        file.truncate()
-        file.write(wsa_build_ver)
-        # Write Github Environment
-        msg = 'Update WSA Version from `v' + currentver + '` to `v' + wsa_build_ver + '`'
-        with open(env_file, "a") as wr:
-            wr.write("SHOULD_BUILD=yes\nRELEASE_TYPE=" + release_type + "\nMSG=" + msg)
-    file.close()
 
-# Get user_code (Thanks to @bubbles-wow because of his repository)
-users = {""}
-try:
-    response = requests.get("https://api.github.com/repos/bubbles-wow/WSAUpdateChecker/contents/token.conf")
-    if response.status_code == 200:
-        content = response.json()["content"]
-        content = content.encode("utf-8")
-        content = base64.b64decode(content)
-        text = content.decode("utf-8")
-        user_code = Prop(text).get("user_code")
-        updatetime = Prop(text).get("update_time")
-        print("Successfully get user token from server!")
-        print(f"Last update time: {updatetime}\n")
-    else:
-        user_code = ""
-        print(f"Failed to get user token from server! Error code: {response.status_code}\n")
-except:
-    user_code = ""
+        content = xmltodict.parse(html.unescape(response.text))
+        product_list = content["s:Envelope"]["s:Body"]["SyncUpdatesResponse"] \
+                              ["SyncUpdatesResult"]["ExtendedUpdateInfo"]["Updates"]["Update"] 
 
-if user_code == "":
-    users = {""}
-else:
-    users = {"", user_code}
-for user in users:
-    if user == "":
-        print("Checking WSA Stable version...\n")
-        if WSAChecker(user, "retail") == 1:
-            break
-        print("Checking Magisk version...\n")
-        if Magisk_Ksu_Gapps_Checker("magisk") == 1:
-            break
-        print("Checking KernelSU version...\n")
-        if Magisk_Ksu_Gapps_Checker("kernelsu") == 1:
-            break
-        print("Checking MindTheGapps version...\n")
-        if Magisk_Ksu_Gapps_Checker("gapps") == 1:
-            break
-    else:
-        print("Checking WSA Insider version...\n")
-        if WSAChecker(user, "WIF") == 1:
-            break
+        for product in product_list:
+            if product["ID"] == self.product_id:
+                package = product["Xml"]["Files"]["File"][0] \
+                                ["@InstallerSpecificIdentifier"]
+                version = re.search("(?<=Android_).*(?=_neutral)", package).group()  # type: ignore
+
+                return version
+
+    def write_to_github_environment(self) -> None:
+        with open(ENV_FILE_PATH, "a") as env_file:
+            env_file.write(
+                f"""
+                SHOULD_BUILD=yes
+                RELEASE_TYPE={self.update_channel}
+                MSG={self.update_msg}
+                """.strip()
+            )
+
+    def overwrite_current_version(self) -> None:
+        with open(self.current_version_file_path, "w") as current_version_file:
+            current_version_file.write(self.latest_version)
+
+
+magisk_update_checker = UtilUpdateChecker(
+    app_id="magisk",
+    app_name="Magisk",
+    latest_version_url="https://github.com/topjohnwu/magisk-files/raw/master/stable.json",
+    json_keys=("magisk", "version"),
+)
+
+gapps_update_checker = UtilUpdateChecker(
+    app_id="gapps",
+    app_name="MindTheGapps",
+    latest_version_url="https://api.github.com/repos/YT-Advanced/MindTheGappsBuilder/releases/latest",
+    json_keys=("name",),
+)
+
+kernelsu_update_checker = UtilUpdateChecker(
+    app_id="kernelsu",
+    app_name="KernelSU",
+    latest_version_url="https://api.github.com/repos/tiann/KernelSU/releases/latest",
+    json_keys=("name",),
+)
+
+wsa_retail_update_checker = WSAUpdateChecker(
+    app_name="WSA",
+    update_channel="retail",
+)
+
+wsa_wif_update_checker = WSAUpdateChecker(
+    app_name="WSA Insider Preview",
+    update_channel="WIF",
+)
+
+
+def main() -> None:
+
+    print("Checking WSA Stable version...", end="\n")
+    if not wsa_retail_update_checker.is_up_to_date():
+        wsa_retail_update_checker.get_update()
+        return
+
+    print("Checking WSA Insider version...", end="\n")
+    if not wsa_wif_update_checker.is_up_to_date():
+        wsa_retail_update_checker.get_update()
+        return
+
+    print("Checking Magisk version...", end="\n")
+    if not magisk_update_checker.is_up_to_date():
+        magisk_update_checker.get_update()
+        return
+
+    print("Checking MindTheGapps version...", end="\n")
+    if not gapps_update_checker.is_up_to_date():
+        gapps_update_checker.get_update()
+        return
+
+    print("Checking KernelSU version...", end="\n")
+    if not kernelsu_update_checker.is_up_to_date():
+        kernelsu_update_checker.get_update()
+        return
+
+
+if __name__ == "__main__":
+    main()
