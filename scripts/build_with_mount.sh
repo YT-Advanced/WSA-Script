40d95918936553eccc030b91e8edf55f3b5a88bf
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-09-15 11:03:48 +0700

    feat(VPNDialogs): Add back `VPNDialogs` to match with lspinit build (#1685)

--- scripts/build_with_mount.sh
+++ scripts/build_with_mount.sh
@@ -607,6 +607,15 @@ elif [ "$ROOT_SOL" = "kernelsu" ]; then
     echo -e "Copy KernelSU kernel done\n"
 fi
 
+echo "Add extra packages"
+sudo cp -r "../common/system/"* "$SYSTEM_MNT" || abort
+find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo rm -rf "$SYSTEM_MNT/priv-app/placeholder/oat"
+find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -type d -exec chmod 0755 {} \;
+find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -type f -exec chmod 0644 {} \;
+find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -exec chown root:root {} \;
+find "../common/system/priv-app/" -maxdepth 1 -mindepth 1 -printf '%P\n' | xargs -I placeholder sudo find "$SYSTEM_MNT/priv-app/placeholder" -exec setfattr -n security.selinux -v "u:object_r:system_file:s0" {} \; || abort
+echo -e "Add extra packages done\n"
+
 echo "Permissions management Netfree and Netspark security certificates"
 sudo cp -r "../cacerts/"* "$SYSTEM_MNT/etc/security/cacerts/" || abort
 sudo chmod 0755 "$SYSTEM_MNT/etc/security/cacerts/"
